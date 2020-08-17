1f82c7b72defa1c32a8907f90bdd29f489d2c515
Subham Tripathi <51919515+subham1093@users.noreply.github.com>
2020-08-17 16:30:03 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # Importing essential libraries
 from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
 import pickle
 
 # Load the Multinomial Naive Bayes model and CountVectorizer object from disk
@@ -15,12 +17,23 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    global df
+    
+    input_features = [request.form.values()]
+    feature_value = np.array(input_features)
+    
     if request.method == 'POST':
     	message = request.form['message']
     	data = [message]
     	vect = cv.transform(data).toarray()
     	my_prediction = classifier.predict(vect)
-    	return render_template('result.html', prediction=my_prediction)
+        #return render_template('index.html', prediction=my_prediction)
+
+    
+    df = pd.concat([df,pd.DataFrame({'employee_information':feature_value,'Predicted Review':my_prediction})],ignore_index=True)
+    df.to_csv('output_data')
+    
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
